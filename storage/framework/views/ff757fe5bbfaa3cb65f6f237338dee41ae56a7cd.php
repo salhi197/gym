6ce08a37addc5841fde0e6b2149dd52e3b911465
WalidLasnami <salhiali197@hotmail.com>
2021-03-27 18:09:29 +0100

    Add column sang to members

---
+++ storage/framework/views/ff757fe5bbfaa3cb65f6f237338dee41ae56a7cd.php
@@ -0,0 +1,131 @@
+<?php $__env->startSection('styles'); ?>
+<link rel="stylesheet" href="<?php echo e(asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')); ?>">
+<link rel="stylesheet" href="<?php echo e(asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')); ?>">
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('header'); ?>
+    <div class="content-header">
+      <div class="container">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+          <a href="<?php echo e(route('membre.create')); ?>" class="btn btn-info"><i class="fafa-plus"></i> Ajouter</a>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+<?php $__env->stopSection(); ?>
+
+
+<?php $__env->startSection('content'); ?>
+
+
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table id="example1" class="table table-striped table-bordered no-wrap">
+                                        <thead>
+                                            <tr>
+                                                <th>id</th>
+                                                <th>nom</th>
+                                                <th>Prénom</th>
+                                                <th>telephone</th>
+                                                <th>Date Début</th>
+                                                <th>status</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php $__currentLoopData = $membres; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $membre): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <tr>
+                                                <td><?php echo e($membre->id); ?></td>
+                                                <td><?php echo e($membre->nom ?? ''); ?></td>
+                                                <td><?php echo e($membre->prenom ?? ''); ?></td>
+                                                <td><?php echo e($membre->telephone ?? ''); ?></td>                                            
+                                                <td>
+                                                    <?php if($membre->etat == 0): ?>
+                                                    <span class="badge badge-danger">Inactive</span>
+                                                    <?php else: ?>
+                                                    <span class="badge badge-info">Inactive</span>                                                    
+                                                    <?php endif; ?>
+                                                <?php echo e($membre->status ?? ''); ?></td>                                            
+                                                <td><?php echo e($membre->created_at ?? ''); ?></td>                                            
+                                                <td>
+                                                <a class="btn btn-info text-white" href="<?php echo e(route('membre.edit',['membre'=>$membre->id])); ?>"><i class="fa fa-edit"></i></a>
+                                                <!-- <a href="<?php echo e(route('membre.destroy',['membre'=>$membre->id])); ?>" onclick="return confirm('Etes vous sure ?')"  class="btn btn-danger text-white"><i class="fa fa-window-close"></i></a> -->
+                                                <a class="btn btn-info text-white" href="<?php echo e(route('membre.inscriptions',['membre'=>$membre->id])); ?>">Inscriptions</a>
+                                                <a class="btn btn-primary text-white" href="<?php echo e(route('membre.facture',['membre'=>$membre->id])); ?>">Facture</a>
+                                                </td>
+                                            </tr>
+
+                                            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>                                            
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('scripts'); ?>
+<script src="<?php echo e(asset('adminlte/plugins/datatables/jquery.dataTables.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-responsive/js/dataTables.responsive.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-responsive/js/responsive.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-buttons/js/dataTables.buttons.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-buttons/js/buttons.bootstrap4.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/jszip/jszip.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/pdfmake/pdfmake.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/pdfmake/vfs_fonts.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-buttons/js/buttons.html5.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-buttons/js/buttons.print.min.js')); ?>"></script>
+<script src="<?php echo e(asset('adminlte/plugins/datatables-buttons/js/buttons.colVis.min.js')); ?>"></script>
+
+
+<script>
+$(document).ready(function() {
+
+          $("#example1").DataTable({
+            "responsive": true, "lengthChange": false, "autoWidth": false,
+            "order": [[ 0, "desc" ]],
+            "buttons": ["copy", "csv", "excel", "pdf", "print"]
+          }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+          $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "responsive": true,
+          });
+
+
+
+        var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+
+        
+        $('.state').on('change',function(e){
+            var id = this.id
+            console.log(id)
+
+            $.ajax({
+                type: 'GET', //THIS NEEDS TO BE GET
+                url: '/membre/state/'+id,
+                dataType: 'JSON',
+                success: function (data) {
+                    console.log(data)
+                    toastr.success('état changé');
+                },
+                error: function(err) { 
+                    console.log(err)
+                    toastr.error(err)
+                }
+            });
+        })
+});
+
+</script>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
